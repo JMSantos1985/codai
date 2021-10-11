d5f8b4f94065b151f67a95fe53f87a704786b40f
jonatas <jonatsmisael21@hotmail.com>
2021-10-11 20:07:24 -0300

    Primeiro commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var cargo = "TI - HSS Informática";
+
+var cargonohtml = document.getElementById(cargo-no-html);
+
+function colocarcargonohtml (cargo){
+     cargonohtml.innerHTML = cargo;
+}
+
+colocarcargonohtml(cargo);
